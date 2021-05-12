758cd7b6512e7e0c296ee456d2fc9ebb83e10ad3
Jeam Carlos Seijas Caña <jeamcarlos.300@gmail.com>
2021-05-12 07:11:59 -0500

    Versión 1.0

---
+++ scripts/ejerciciobase.js
@@ -0,0 +1,23 @@
+function ComprobarPalindromo(texto){
+
+	let textoLimpio = texto.toLowerCase().replace(/[\W_]/g, "");
+	let cantidadLetras = textoLimpio.length;
+	let ultimaLetra = textoLimpio.length - 1;
+	let verificar = false;
+
+	for (var i = 0; i < ultimaLetra; i++) {
+		if (textoLimpio[i] == textoLimpio[ultimaLetra]) {
+			verificar = true;
+			ultimaLetra --;
+		}else{
+			verificar = false;
+		}
+		
+	}
+
+	return verificar;
+  
+}
+
+var palindromo = ComprobarPalindromo("almostomla");
+console.log(palindromo);
---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+var cajaPalindromo = document.getElementById("caja-texto");
+var cajaForm = document.getElementById("caja-form");
+var cajaLoad = document.getElementById("caja-loading");
+var cajaMensaje = document.getElementById("caja-mensaje");
+var palabra = document.getElementById("palindromo");
+var mensaje = document.getElementById("mensaje");
+var valorPalindromo, texto; 
+
+
+function MostrarResultado(){
+	cajaLoad.style.display = "none";
+	cajaMensaje.style.display = "block";
+	
+}
+function Reiniciar(){
+	cajaPalindromo.value = "";
+
+	cajaMensaje.style.display = "none";
+	cajaForm.style.display = "block";
+}
+
+function ComprobarPalindromo(){
+	
+	valorPalindromo = cajaPalindromo.value;
+
+	if (valorPalindromo == "") {
+		alert("Debes Agregar una Palabra")
+	}else{
+	let textoLimpio = valorPalindromo.toLowerCase().replace(/[\W_]/g, "");
+	let cantidadLetras = textoLimpio.length;
+	let ultimaLetra = textoLimpio.length - 1;
+	let verificar = false;
+
+	cajaForm.style.display = "none";
+	cajaLoad.style.display = "block";
+
+	setTimeout(MostrarResultado, 1500);
+
+	for (var i = 0; i < ultimaLetra; i++) {
+		if (textoLimpio[i] == textoLimpio[ultimaLetra]) {
+			verificar = true;
+			ultimaLetra --;
+		}else{
+			verificar = false;
+		}
+		
+	}
+
+	if (verificar == true) {
+		texto = textoLimpio.toUpperCase();
+		palabra.innerHTML = texto;
+		mensaje.innerHTML = "Si es un Palíndromo";
+	}else{
+		texto = textoLimpio.toUpperCase();
+		palabra.innerHTML = texto;
+		mensaje.innerHTML = "No es un Palindromo";
+	}
+	}
+  
+}
